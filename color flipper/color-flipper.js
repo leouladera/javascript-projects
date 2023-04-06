0c3b0a81151f5afe145b3c2e095a37dbac17ee17
leouladera <Rover7614@gmail.com>
2023-04-06 06:26:36 +0300

    COLOR flipper project

---
+++ color flipper/color-flipper.js
@@ -0,0 +1,25 @@
+const colors = [
+  "green",
+  "red",
+  "rgba(133, 122, 200)",
+  "#f15025",
+  "blue",
+  "yellow",
+  "blanchedalmond",
+  "light green",
+  "black",
+  "orange",
+  "purple",
+  "pink",
+];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
