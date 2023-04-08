e03de94f017367a4893667479d846bedd8d41ddb
leouladera <Rover7614@gmail.com>
2023-04-08 12:55:58 +0300

    added reset feature

--- 01-color flipper/color-flipper.js
+++ 01-color flipper/color-flipper.js
@@ -13,13 +13,18 @@ const colors = [
   "pink",
 ];
 const btn = document.getElementById("btn");
+const reset = document.getElementById("reset");
 const color = document.querySelector(".color");
+const rest = document.querySelector(".rst");
 btn.addEventListener("click", function () {
   const randomNumber = getRandomNumber();
-  console.log(randomNumber);
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 });
 function getRandomNumber() {
   return Math.floor(Math.random() * colors.length);
 }
+reset.addEventListener("click", function () {
+  document.body.style.backgroundColor = "#F1f5f8";
+  color.textContent = "#F1f5f8";
+});
--- 01-color flipper/hex.js
+++ 01-color flipper/hex.js
@@ -1,6 +1,7 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+const rest = document.getElementById("reset");
 btn.addEventListener("click", function () {
   let hexcolor = "#";
   for (let i = 0; i < 6; i++) {
@@ -12,3 +13,7 @@ btn.addEventListener("click", function () {
 function getRandomNumber() {
   return Math.floor(Math.random() * hex.length);
 }
+rest.addEventListener("click", function () {
+  document.body.style.backgroundColor = "#F1f5f8";
+  color.textContent = "#F1f5f8";
+});
