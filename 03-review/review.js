707448a193f6c027d65ed21b60792330c11bdb6b
leouladera <Rover7614@gmail.com>
2023-04-11 04:08:17 +0300

    raiting Added

--- 03-review/review.js
+++ 03-review/review.js
@@ -6,7 +6,7 @@ const reviews = [
     job: "web developer",
     img: "https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg",
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-    rating: "🌟🌟🌟🌟",
+    rating: "⭐⭐⭐",
   },
   {
     id: 2,
@@ -14,7 +14,7 @@ const reviews = [
     job: "web designer",
     img: "https://images2.imgbox.com/2e/6e/JAMvTZ56_o.jpeg",
     text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-    rating: "🌟🌟🌟🌟",
+    rating: "⭐⭐⭐⭐",
   },
   {
     id: 3,
@@ -22,7 +22,7 @@ const reviews = [
     job: "intern",
     img: "https://images2.imgbox.com/56/88/oJvFN3l5_o.jpeg",
     text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-    rating: "🌟🌟🌟🌟🌟",
+    rating: "⭐⭐⭐⭐⭐",
   },
   {
     id: 4,
@@ -39,6 +39,7 @@ const img = document.getElementById("person-img");
 const author = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
+const rate = document.getElementById("str");
 
 const btns = document.querySelector(".button-container");
 const prevBtn = document.querySelector(".prev-btn");
@@ -64,7 +65,7 @@ function showPerson() {
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
-  Str.textContent = reviews[currentItem].rating;
+  rate.textContent = reviews[currentItem].rating;
 }
 // show next person
 nextBtn.addEventListener("click", function () {
