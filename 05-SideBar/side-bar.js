3f868ba42c99431cd03a24c681bf020d6c6e49e6
leouladera <Rover7614@gmail.com>
2023-05-03 06:51:24 +0300

    SideBar

---
+++ 05-SideBar/side-bar.js
@@ -0,0 +1,10 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+  sidebar.classList.add("show-sidebar");
+});
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
