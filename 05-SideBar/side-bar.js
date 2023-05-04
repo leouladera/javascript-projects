0fb464cc008db6fb5e1807d778cc4163bf74125b
leouladera <Rover7614@gmail.com>
2023-05-04 04:32:41 +0300

    fixed button

--- 05-SideBar/side-bar.js
+++ 05-SideBar/side-bar.js
@@ -3,7 +3,7 @@ const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
 toggleBtn.addEventListener("click", function () {
-  sidebar.classList.add("show-sidebar");
+  sidebar.classList.toggle("show-sidebar");
 });
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
